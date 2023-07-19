4e1048321cf3c6f2eac39284ccb406162a53ddc5
Jean Vivien <akouvivien@gmail.com>
2023-07-19 13:15:41 +0000

    amelioration des modeles et des views avec prise en compte de la date de modification, creation de l'utilisateur etc. 1

--- crudProjet/settings.py
+++ crudProjet/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-   # 'djanho_filters',
-    'crudProjet_api'
+    'crudProjet_api',
+    # 'djanho_filters',
+    #'auditlog',
+ 
 ]
 
 MIDDLEWARE = [
